32dafca9ecb41d28a5a1be013fa2033d6e511481
Mark Beukers <mark@afrotoss.com>
2016-12-21 20:18:14 +0800

    Replace R.toString with Z.toString

--- test/internal/EitherArb.js
+++ test/internal/EitherArb.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var jsc = require('jsverify');
-var R = require('ramda');
 
 var S = require('../..');
 
+var Z = require('sanctuary-type-classes');
 
 //  EitherArb :: Arbitrary a -> Arbitrary b -> Arbitrary (Either a b)
 module.exports = function EitherArb(lArb, rArb) {
-  return jsc.oneof(lArb.smap(S.Left, S.prop('value'), R.toString),
-                   rArb.smap(S.Right, S.prop('value'), R.toString));
+  return jsc.oneof(lArb.smap(S.Left, S.prop('value'), Z.toString),
+                   rArb.smap(S.Right, S.prop('value'), Z.toString));
 };
--- test/internal/IdentityArb.js
+++ test/internal/IdentityArb.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var R = require('ramda');
-
 var S = require('../..');
+
+var Z = require('sanctuary-type-classes');
 
 var Identity = require('./Identity');
 
 
 //  IdentityArb :: Arbitrary a -> Arbitrary (Identity a)
 module.exports = function IdentityArb(arb) {
-  return arb.smap(Identity, S.prop('value'), R.toString);
+  return arb.smap(Identity, S.prop('value'), Z.toString);
 };
